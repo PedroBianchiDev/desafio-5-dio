078a99fbde0bcf583a146693d53ff12c66d58e27
Pedro Bianchi <145021842+PedroBianchiDev@users.noreply.github.com>
2024-10-12 17:14:28 -0300

    Delete services/api.js

--- services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: 'https://vabcixwnqthdykbqcmgl.supabase.co/rest/v1/',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhYmNpeHducXRoZHlrYnFjbWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NTc4NTMsImV4cCI6MjA0NDMzMzg1M30.GKwjhSChwNQdyN4eYnlE41WfQxLtrkpROEUqxlcRz8Y",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhYmNpeHducXRoZHlrYnFjbWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NTc4NTMsImV4cCI6MjA0NDMzMzg1M30.GKwjhSChwNQdyN4eYnlE41WfQxLtrkpROEUqxlcRz8Y"
-    }
-})
